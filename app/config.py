8b030ea4390bbbea6333ce1acebf9b58e574fdca
WEGFan <goofans@qq.com>
2020-07-27 00:14:59 +0800

    update: simplify path construct

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config(object):
     APP_PATH: str = str(Path(__file__).parent.resolve())
     PROJECT_PATH: str = str(Path(APP_PATH).parent.resolve())
 
-    LOG_PATH: str = str((Path(PROJECT_PATH) / 'data' / 'logs').resolve())
+    LOG_PATH: str = str(Path(PROJECT_PATH, 'data', 'logs').resolve())
     LOG_FILENAME: str = 'app.log'
     LOG_FORMAT: str = '[%(asctime)s] %(levelname)s - [%(module)s] [%(filename)s:%(lineno)s]: %(message)s'
     LOG_LEVEL: int = logging.INFO
